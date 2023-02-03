16b77cf8f6f3d6168dd158131385668565c1db64
liaojiayun <liaojiayun@bytedance.com>
2023-02-03 11:02:09 +0800

    feat: modify babel-loader

--- build/webpack.base.ts
+++ build/webpack.base.ts
@@ -26,28 +26,7 @@ const baseConfig: Configuration = {
     rules: [
       {
         test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
-        use: {
-          loader: "babel-loader",
-          options: {
-            // 预设执行顺序由右往左,所以先处理ts,再处理jsx
-            presets: [
-                [
-                  "@babel/preset-env",
-                  {
-                    // 设置兼容目标浏览器版本,这里可以不写,babel-loader会自动寻找上面配置好的文件.browserslistrc
-                    targets: { browsers: ["> 1%", "last 2 versions", "not ie <= 8"] },
-                    useBuiltIns: "usage", // 根据配置的浏览器兼容,以及代码中使用到的api进行引入polyfill按需添加
-                    corejs: 3, // 配置使用core-js使用的版本
-                    loose: true,
-                  },
-                ],
-                // 如果您使用的是 Babel 和 React 17，您可能需要将 "runtime": "automatic" 添加到配置中。
-                // 否则可能会出现错误：Uncaught ReferenceError: React is not defined
-                ["@babel/preset-react", { runtime: "automatic" }],
-                "@babel/preset-typescript",
-              ],
-          },
-        },
+        use: "babel-loader"
       },
       {
         test: /.css$/, //匹配 css 文件
