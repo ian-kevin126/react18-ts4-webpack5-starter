1ac393b89d60ce9a8dae10776a29d1e8cf8b26da
liaojiayun <liaojiayun@bytedance.com>
2023-02-24 15:53:09 +0800

    feat: add index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './index.css'
 // import '@/assets/css/iconfont.css'
 
 // const root = document.getElementById('root');
