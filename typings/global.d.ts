0b923a23a518766d7d2bac84e2980c82bdeb9892
liaojiayun <liaojiayun@bytedance.com>
2023-02-24 11:21:19 +0800

    feat: modify spelling

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -13,37 +13,63 @@ declare module 'process' {
 /* Use this folder to also add/extend a package d.ts file, if needed. */
 
 /* CSS MODULES */
-declare module '*.module.css' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
+// declare module '*.module.css' {
+//   const classes: { [key: string]: string };
+//   export default classes;
+// }
 
-declare module '*.module.scss' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
+// declare module '*.module.scss' {
+//   const classes: { [key: string]: string };
+//   export default classes;
+// }
 
-declare module '*.module.sass' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
+// declare module '*.module.sass' {
+//   const classes: { [key: string]: string };
+//   export default classes;
+// }
 
-declare module '*.module.less' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
+// declare module '*.less' {
+//   const classes: { [key: string]: string };
+//   export default classes;
+// }
 
-declare module '*.module.styl' {
-  const classes: { [key: string]: string };
-  export default classes;
-}
+// declare module '*.module.styl' {
+//   const classes: { [key: string]: string };
+//   export default classes;
+// }
 
 /* CSS */
-declare module '*.css';
-declare module '*.scss';
-declare module '*.sass';
-declare module '*.less';
-declare module '*.styl';
+// declare module '*.css';
+// declare module '*.scss';
+// declare module '*.sass';
+// declare module '*.less';
+// declare module '*.styl';
+
+/* CSS MODULES */
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.scss' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.sass' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.less' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.styl' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
 
 /* IMAGES */
 declare module '*.svg' {
